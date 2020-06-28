9afe984be8f527d6ae9c86a595b01a5cf6bbb279
Abednego Kipkirui <abed@Flow-mac.local>
2020-06-27 21:28:30 -0500

    adding another page

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 #Default page
 @app.route("/")
-#when user goes to my website, active function below
+#when user goes to my website, activate function below
 def home():
     return "Hello, World!"
+
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
 #when script is run, python assigns name "__main__" to script when executed
 if __name__ == "__main__":
     app.run(debug=True)
